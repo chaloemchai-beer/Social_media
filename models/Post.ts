bcb2ab0858bd7247ef98883276927d93e4d01252
chaloemchai-beer <Kasutochose1@gmail.com>
2024-09-13 00:01:45 +0700

    mongodb add post

--- models/Post.ts
+++ models/Post.ts
@@ -1,21 +1,11 @@
 import mongoose from 'mongoose';
 
-const PostSchema = new mongoose.Schema({
-  text: { type: String, required: false },
-  videoUrl: { type: String, required: false },
-  imageUrl: { type: String, required: false },
-  name: { type: String, required: true },
-  message: { type: String, required: false },
+const postSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  imageUrl: { type: String, required: false }, // Optional
+  videoUrl: { type: String, required: false }, // Optional
   email: { type: String, required: true },
-  postImage: { type: String, required: false },
-  image: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
 });
 
-PostSchema.pre('save', function(next) {
-  this.updatedAt = new Date();
-  next();
-});
-
-export default mongoose.models.Post || mongoose.model('Post', PostSchema);
+export default mongoose.model('Post', postSchema);
