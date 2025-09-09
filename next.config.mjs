84ede838725ec58c23c50f14b5c2b48f36043ec3
Chaloemchai-beer <Chaloemchai.ai@gmail.com>
2025-09-09 13:46:05 +0700

    test deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+const isDev = process.env.NODE_ENV !== 'production'
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    unoptimized: isDev,
+    remotePatterns: [
+      { protocol: 'https', hostname: 'zpbwjwikdzxcseumoiwk.supabase.co', pathname: '/**' },
+      { protocol: 'https', hostname: 'archive.smashing.media', pathname: '/**' },
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'cloudflare-ipfs.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'picsum.photos', pathname: '/**' },
+      { protocol: 'https', hostname: 'loremflickr.com', pathname: '/**' },
+    ],
+  },
+}
 
 export default nextConfig;
